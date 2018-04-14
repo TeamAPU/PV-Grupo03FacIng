a59005370d5e2108a3a5d257127b4855f449e3cf
shizukaddf <38300010+shizukaddf@users.noreply.github.com>
2018-04-14 19:23:52 -0300

    Creacion de los metodos superficieRectangulo y perimetroRectangulo en la clase Rectangulo por David Farfán

--- src/aplicacion/dominio/Rectangulo.java
+++ src/aplicacion/dominio/Rectangulo.java
@@ -11,4 +11,50 @@ package aplicacion.dominio;
  */
 public class Rectangulo {
     
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+    
+    public double superficieRectangulo(){
+        
+        return base*altura;
+
+    }
+    
+    public double perimetroRectangulo(){
+        
+        return base * 2 + altura*2;
+        
+    } 
+
+   
+    public int posicionPuntos(int x,int y){
+                
+        return 0;
+    }
+
+    
+    
+    
 }
