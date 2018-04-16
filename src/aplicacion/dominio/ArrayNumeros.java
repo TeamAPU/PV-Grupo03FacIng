3d0e365c62c756a387ae432e8955eb0df8764e9c
pc <pc@EUROCASE>
2018-04-16 00:04:21 -0300

    Agregado de la clase ArrayNumeros del punto 4 tp3 by Baseball90

---
+++ src/aplicacion/dominio/ArrayNumeros.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.dominio;
+
+/**
+ *
+ * @author Carolina
+ */
+public class ArrayNumeros {
+    private int arregloNumeros[];
+    
+    public ArrayNumeros(){
+        
+    }
+
+    public int[] getArregloNumeros() {
+        return arregloNumeros;
+    }
+
+    public void setArregloNumeros(int[] arregloNumeros) {
+        this.arregloNumeros = arregloNumeros;
+    }
+    
+    public int obtenerMayor(){
+         int mayor = arregloNumeros[0];
+        for (int i = 1; i < arregloNumeros.length; i++) {
+            if (mayor<arregloNumeros[i]) {
+                mayor=arregloNumeros[i];
+            }
+        }
+        return mayor;
+        
+        
+    }
+    
+    
+    public int obtenerMenor(){
+         int menor = arregloNumeros[0];
+        for (int i = 1; i < arregloNumeros.length; i++) {
+            if (menor>arregloNumeros[i]) {
+                menor=arregloNumeros[i];
+            }
+        }
+        
+        return menor;
+    }
+    
+     public int obtenerPromedio(){
+        int suma=0;
+        for (int i = 0; i < arregloNumeros.length; i++) {
+            suma = suma + arregloNumeros[i];
+        }
+        return suma/arregloNumeros.length;
+    }
+    
+      public void mostrarElementosDelArreglo(){
+        for(int i=0; i< arregloNumeros.length; i++){
+            System.out.println("Valor del elemento: "+ arregloNumeros[i]);
+        }
+    }
+    
+}
