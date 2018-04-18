4e3ff9988551410ff6b104c7e4c6d07b602b427b
pc <pc@EUROCASE>
2018-04-17 23:19:46 -0300

    La Clase ArrayNumeros fue comentada para no borrarla ya que es correcta solo esta de referencia con respecto al punto 4 del tp3

--- src/aplicacion/dominio/ArrayNumeros.java
+++ src/aplicacion/dominio/ArrayNumeros.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aplicacion.dominio;
+/**package aplicacion.dominio;
 
 /**
  *
  * @author Carolina
  */
-public class ArrayNumeros {
+/**public class ArrayNumeros {
     private int arregloNumeros[];
     
     public ArrayNumeros(){
